1001a2797a960bfb8027e89e555d0a54fce48966
abderrahim boudounit <abderrahimboudounit@gmail.com>
2024-01-23 22:45:25 +0100

    adding ev variables

--- mongodbtest/app/api/user/route.ts
+++ mongodbtest/app/api/user/route.ts
@@ -5,12 +5,15 @@ import {MongoClient} from "mongodb";
 
 export async function GET() {
        await dbConnect();
-       const url = "mongodb+srv://abderrahim:134237@cluster0.ne1hqkp.mongodb.net/metaverseTest?retryWrites=true&w=majority";
+       const url = process.env.MONGODB_URI;
+       const db_collection = process.env.MONGODB_COLLECTION
+       if (!url || !db_collection)
+              return;
        const client = new MongoClient(url);
        await client.connect();
-       const db = client.db("metaverseTest");
+       const db = client.db(process.env.MONGODB_DB);
        const user = await db
-       .collection("user")
+       .collection(db_collection)
        .find({})
        .toArray();
        return NextResponse.json(user);
